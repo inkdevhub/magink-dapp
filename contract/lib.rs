817e51ccbc2b0006393e8cc00d4aeaa3526884c6
Mar.io <34627453+Maar-io@users.noreply.github.com>
2023-06-02 19:32:14 +0200

    magink contract

--- contract/lib.rs
+++ contract/lib.rs
@@ -2,10 +2,34 @@
 
 #[ink::contract]
 pub mod magink {
+    use crate::ensure;
+    use ink::storage::Mapping;
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        TooEarlyToClaim,
+        UserNotFound,
+    }
+
     #[ink(storage)]
     pub struct Magink {
-        blocks_to_live: u8,
+        user: Mapping<AccountId, Profile>,
+    }
+    #[derive(
+        Debug, PartialEq, Eq, PartialOrd, Ord, Clone, scale::Encode, scale::Decode,
+    )]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct Profile {
+        // duration in blocks until next claim
+        claim_era: u8,
+        // block number of last claim
         start_block: u32,
+        // number of badges claimed
+        badges_claimed: u8,
     }
 
     impl Magink {
@@ -13,32 +37,57 @@ pub mod magink {
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {
-                blocks_to_live: 0,
-                start_block: 0,
+                user: Mapping::new(),
             }
         }
 
-        /// Set the eras of the Magink smart contract.
+        /// (Re)Start the Magink the claiming era for the caller.
         #[ink(message)]
-        pub fn start(&mut self, life_span: u8) {
-            self.blocks_to_live = life_span;
-            self.start_block = self.env().block_number();
+        pub fn start(&mut self, era: u8) {
+            let profile = Profile {
+                claim_era: era,
+                start_block: self.env().block_number(),
+                badges_claimed: 0,
+            };
+            self.user.insert(self.env().caller(), &profile);
         }
 
-        /// Water the plant
+        /// Claim the badge after the era.
         #[ink(message)]
-        pub fn water(&mut self) {
-            self.start_block = self.env().block_number();
+        pub fn claim(&mut self) -> Result<(), Error> {
+            ensure!(self.get_remaining() > 0, Error::TooEarlyToClaim);
+
+            // update profile
+            let mut profile = self.get_profile().ok_or(Error::UserNotFound).unwrap();
+            profile.badges_claimed += 1;
+            profile.start_block = self.env().block_number();
+            self.user.insert(self.env().caller(), &profile);
+            Ok(())
         }
 
-        /// Returns the current value of the Magink's boolean.
+        /// Returns the remaining blocks in the era.
         #[ink(message)]
-        pub fn get_water(&self) -> u8 {
+        pub fn get_remaining(&self) -> u8 {
             let current_block = self.env().block_number();
-            if current_block - self.start_block >= self.blocks_to_live as u32 {
-                return 0;
-            }
-            self.blocks_to_live - (current_block - self.start_block) as u8
+            let caller = self.env().caller();
+            self.user.get(&caller).map_or(0, |profile| {
+                if current_block - profile.start_block >= profile.claim_era as u32 {
+                    return 0;
+                }
+                profile.claim_era - (current_block - profile.start_block) as u8
+            })
+        }
+
+        /// Returns the profile of the given account.
+        #[ink(message)]
+        pub fn get_account_profile(&self, account: AccountId) -> Option<Profile> {
+            self.user.get(&account)
+        }
+        
+        /// Returns the profile of the caller.
+        fn get_profile(&self) -> Option<Profile> {
+            let caller = self.env().caller();
+            self.user.get(&caller)
         }
     }
 
@@ -49,9 +98,15 @@ pub mod magink {
         #[ink::test]
         fn it_works() {
             let mut magink = Magink::new();
-            println!("get {:?}", magink.get_water());
+            println!("get {:?}", magink.get_remaining());
             magink.start(10);
-            assert_eq!(10, magink.get_water());
+            assert_eq!(10, magink.get_remaining());
+            advance_block();
+            assert_eq!(9, magink.get_remaining());
+        }
+
+        fn advance_block() {
+            ink::env::test::advance_block::<ink::env::DefaultEnvironment>();
         }
     }
 
@@ -115,4 +170,16 @@ pub mod magink {
             Ok(())
         }
     }
+}
+
+/// Evaluate `$x:expr` and if not true return `Err($y:expr)`.
+///
+/// Used as `ensure!(expression_to_ensure, expression_to_return_on_false)`.
+#[macro_export]
+macro_rules! ensure {
+    ( $x:expr, $y:expr $(,)? ) => {{
+        if !$x {
+            return Err($y.into());
+        }
+    }};
 }
