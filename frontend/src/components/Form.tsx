4e095dfe0d2b0efb3ff3a5ea4f0c77e4cb61c8a8
Bobo <bobo.kovacevic@gmail.com>
2023-06-02 15:09:06 +0200

    Form and FormContainer refactoring

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,88 +1,38 @@
-import { DryRunResult } from "./DryRunResult";
-import { Form, Field, ErrorMessage, useFormikContext } from "formik";
-import { Values, UIEvent, TransferredBalanceEvent } from "../types";
-import { ChangeEvent, useState } from "react";
-import { NewUserGuide } from "./NewUserGuide";
-import { useLinkContract, useUI } from "../hooks";
-import { pickDecoded, pickDecodedError, pickError } from "useink/utils";
-import { decodeError } from "useink/core";
-import { useWallet } from "useink";
-import { Button } from "./Button";
-import { get } from "http";
+import { Form, useFormikContext } from 'formik';
+import { Values } from '../types';
+import { useState } from 'react';
+import { NewUserGuide } from './NewUserGuide';
+import { useLinkContract, useUI } from '../hooks';
+import { pickDecodedError } from 'useink/utils';
+import { useWallet } from 'useink';
+import { Button } from './Button';
 
-export const UrlShortenerForm = () => {
+interface Props {
+  awake: () => void;
+  isAwake: boolean;
+  waterLevel: number;
+  runtimeError?: any;
+}
+
+export const UrlShortenerForm = ({ awake, isAwake, waterLevel, runtimeError }: Props) => {
   const { isSubmitting, isValid, values, setFieldTouched, handleChange } = useFormikContext<Values>();
   const { waterDryRun, tamagotchink, start, getWater } = useLinkContract();
   const { account } = useWallet();
   const { setShowConnectWallet } = useUI();
-  const [isAwake, setIsAwake] = useState(false);
-  const [txMessage, setTxMessage] = useState<string>("");
-  const [waterLevel, setWaterLevel] = useState<number>(0);
+  const [txMessage, setTxMessage] = useState<string>('');
 
-  var runtimeError;
-  const checkLevel = () => {
-      const interval = setInterval(async () => {
-        const waterStatus = await getWater?.send([], { defaultCaller: true });
-        console.log("##### getWater value", waterStatus?.ok && waterStatus.value.decoded);
-        
-        if (waterStatus?.ok && waterStatus.value.decoded) {
-          // setTxMessage("Read plant health: " + waterStatus?.value?.decoded);
-          setWaterLevel(waterStatus.value.decoded);
-        }
-
-        runtimeError = pickError(getWater?.result);
-        if (runtimeError != undefined) {
-          console.log("Form getWater runtimeError", runtimeError);
-        }
-      }, 5000);
-      return () => clearInterval(interval);
-  }
-  const awakeTamagotchi = async () => {
-    console.log("awakeTamagotchi");
-    setIsAwake(true);
-
-    const startArgs = [values.blocksToLive];
-    const options = undefined;
-    setTxMessage("Sign awaking transaction");
-    start?.signAndSend(startArgs, options, (result, _api, error) => {
-      if (error) {
-        console.error(JSON.stringify(error));
-      }
-      console.log("result", result);
-      if (result?.status) {
-        setTxMessage("Transaction status: " + result?.status.type);
-      }
-      if (result?.status.isInBlock) {
-        console.log("invoke checkLevel");
-        checkLevel();
-      }
-      const dispatchError = start.result?.dispatchError;
-
-      if (dispatchError && tamagotchink?.contract) {
-        const errorMessage = decodeError(dispatchError, tamagotchink, undefined, 'Something went wrong');
-        console.log("errorMessage", errorMessage);
-      }
-    });
-
-  };
   return (
     <Form>
       <div className="group">
-        {account &&
-          !isAwake && 
-          (
-            <Button
-              type="button"
-              disabled={isSubmitting || !isValid}
-              onClick={awakeTamagotchi}
-            >
-              Awake Tamagotchi
-            </Button>
-          )}
+        {account && !isAwake && (
+          <Button type="button" disabled={isSubmitting || !isValid} onClick={awake}>
+            Awake Tamagotchi
+          </Button>
+        )}
       </div>
 
       <div className="group">
-        Blocks to live{" "}{waterLevel}
+        Blocks to live {waterLevel}
         {/* <ErrorMessage name="alias" component="div" className="error-message" /> */}
       </div>
 
@@ -90,28 +40,19 @@ export const UrlShortenerForm = () => {
         {isValid && <DryRunResult values={values} />}
       </div> */}
 
-
       <div className="group">
         {account ? (
-          <Button
-            type="submit"
-            disabled={isSubmitting || !isValid}
-          >
+          <Button type="submit" disabled={isSubmitting || !isValid}>
             Water
           </Button>
         ) : (
-          <Button 
-          type="button"
-
-          onClick={() => setShowConnectWallet(true)}>
+          <Button type="button" onClick={() => setShowConnectWallet(true)}>
             Connect Wallet
           </Button>
         )}
       </div>
 
-      <div className="text-xs text-left mb-2 text-purple-500">
-        {txMessage}
-      </div>
+      <div className="text-xs text-left mb-2 text-purple-500">{txMessage}</div>
 
       {runtimeError && tamagotchink && (
         <div className="text-xs text-left mb-2 text-red-500">
